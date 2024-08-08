a354bcbe5dc9c6bcac2ce816d7834fad2737fcc6
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2024-08-08 09:09:45 -0300

    08/08/2024

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Olá,Mundo</h1>
+  <select id="Escolhas_Region"></select>
+</template>
+
+<script>
+
+fetch('https://restcountries.com/v3.1/all').then((response) => response.json().then((dados) => {
+  for (var i = 0 ; i < dados.length; i++){
+    var Opt = document.createElement("option")
+    Opt.innerText = `${dados[i].name.common} (${i})`
+    Opt.value = `${dados[i].name.common} (${i})`
+    document.getElementById("Escolhas_Region").append(Opt)
+  }
+}))
+
+
+</script>
